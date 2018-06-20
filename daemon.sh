de0a8679ade5b7484b081798570d9509f77e6feb
yanlong <592938410@qq.com>
2018-06-20 16:15:27 +0800

    Fixed daemon.sh bug

--- daemon.sh
+++ daemon.sh
@@ -8,14 +8,22 @@ set -o verbose
 PWD=$(cd "$(dirname "$0")" && pwd)
 export PATH="$PWD:$PATH"
 
-btcwallet --username=root, --password=1314 --simnet --createtemp --datadir=/root/.btcwallet/datadir &
+gencerts
+
+mkdir -p .btcd .btcwallet
+cp rpc.cert rpc.key .btcwallet/
+cp "$GOPATH"/src/github.com/btcsuite/btcwallet/sample-btcwallet.conf  "${HOME}"/.btcwallet/btcwallet.conf
+cp "$GOPATH"/src/github.com/btcsuite/btcd/sample-btcd.conf "${HOME}"/.btcd/btcd.conf
+cp rpc.cert rpc.key .btcd/
+
+btcwallet --username=root --password=1314 --simnet --createtemp --appdata="${HOME}"/.btcwallet &
 
 sleep 2
 miningaddr=$(btcctl --rpcuser=root --rpcpass=1314 --simnet --wallet getnewaddress)
 
-echo "ming address: ${miningaddr}"
+echo "mingaddress: ${miningaddr}"
 
 btcctl --rpcuser=root --rpcpass=1314 --simnet --wallet listacounts
 sleep 2
 
-btcd --generate --simnet --rpcuser=root --rpcpass=1314 --miningaddr="${miningaddr}" --txindex
+btcd --simnet --rpcuser=root --rpcpass=1314 --miningaddr="${miningaddr}" --txindex
