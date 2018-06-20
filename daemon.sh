36389a10d8b51879f677ad7011e1da50dd770d08
yanlong <592938410@qq.com>
2018-06-20 16:54:09 +0800

    Fixed bug

--- daemon.sh
+++ daemon.sh
@@ -3,27 +3,27 @@
 set -e
 set -o pipefail
 set -o verbose
-#set -o xtrace
+# set -o xtrace
 
 PWD=$(cd "$(dirname "$0")" && pwd)
 export PATH="$PWD:$PATH"
 
-gencerts
+gencerts -f
 
-mkdir -p .btcd .btcwallet
-cp rpc.cert rpc.key .btcwallet/
-cp "$GOPATH"/src/github.com/btcsuite/btcwallet/sample-btcwallet.conf  "${HOME}"/.btcwallet/btcwallet.conf
+mkdir -p "${HOME}"/.btcd "${HOME}"/.btcwallet
+cp rpc.cert rpc.key "${HOME}"/.btcwallet/
+cp "$GOPATH"/src/github.com/btcsuite/btcwallet/sample-btcwallet.conf "${HOME}"/.btcwallet/btcwallet.conf
 cp "$GOPATH"/src/github.com/btcsuite/btcd/sample-btcd.conf "${HOME}"/.btcd/btcd.conf
-cp rpc.cert rpc.key .btcd/
+cp rpc.cert rpc.key "${HOME}"/.btcd/
 
 btcwallet --username=root --password=1314 --simnet --createtemp --appdata="${HOME}"/.btcwallet &
 
-sleep 2
+sleep 10
 miningaddr=$(btcctl --rpcuser=root --rpcpass=1314 --simnet --wallet getnewaddress)
 
 echo "mingaddress: ${miningaddr}"
 
-btcctl --rpcuser=root --rpcpass=1314 --simnet --wallet listacounts
+btcctl --rpcuser=root --rpcpass=1314 --simnet --wallet listaccounts
 sleep 2
 
 btcd --simnet --rpcuser=root --rpcpass=1314 --miningaddr="${miningaddr}" --txindex
